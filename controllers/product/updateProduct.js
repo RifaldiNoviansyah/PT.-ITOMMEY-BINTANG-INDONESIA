b9c67de2ad83ef9f6381ec685fd7a9e9cfcfbf4f
RIFALDI NOVIANSYAH <rifaldinoviansyah11@gmail.com>
2022-09-10 20:13:31 +0700

    web add api update

--- controllers/product/updateProduct.js
+++ controllers/product/updateProduct.js
@@ -1,9 +1,35 @@
+const {tbl_products: productModel} = require('@models');
+const response = require('@helpers/response');
+const {internalServerError, notFound} = require('@helpers/errorResponse');
 module.exports = {
   update: async (req, res) => {
     try {
-      console.log(`update`);
+      const {id} = req.params;
+      const {name, qty, expiredAt} = req.body;
+      const productModelData = await productModel.findOne({
+        where: {id, is_active: true},
+        attributtes: ['id', 'name', 'qty', 'picture', 'expired_at', 'is_active'],
+      });
+      if (!productModelData) return notFound(res, 'product Not Found');
+
+      productModelData.name = name;
+      productModelData.qty = qty;
+      productModelData.expired_at = expiredAt;
+      await productModelData.save();
+
+      const result = {
+        id: productModelData.id,
+        name: productModelData.name,
+        qty: productModelData.qty,
+        picture: null,
+        expiredAt: productModelData.expired_at,
+        isActive: productModelData.is_active,
+      };
+
+      return response(res, 200, 'Updated Product Success', result);
     } catch (error) {
       console.log(error);
+      return internalServerError(res, error.message);
     }
   },
 };
