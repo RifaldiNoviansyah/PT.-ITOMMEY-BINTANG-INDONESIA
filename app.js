6883160285393d5da44ddb827c19cf3cc4917451
RIFALDI NOVIANSYAH <rifaldinoviansyah11@gmail.com>
2022-09-10 20:44:18 +0700

    set cors

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ const path = require('path');
 const db = require('@helpers/database.js');
 const apiRoute = require('./routes/api/apiRouteIndex');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: false, limit: '50mb'}));
 app.use(express.static(path.join(__dirname, 'file')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, HEAD, PATCH, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, X-Access-Token, X-Key');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 app.use('/', apiRoute);
 app.get('*', (req, res) => res.status(200).send(
     {message: 'Welcome to API Test PT ITOMMEY BINTANG INDONESIA'}));
-
 
 db.authenticate()
     .then(() => {
