883c487b42a2623e79ea68b8b7ce90d709675a99
RIFALDI NOVIANSYAH <rifaldinoviansyah11@gmail.com>
2022-09-10 19:46:50 +0700

    add api create product

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const db = require('@helpers/database.js');
 const apiRoute = require('./routes/api/apiRouteIndex');
 const app = express();
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: false, limit: '50mb'}));
 app.use('/', apiRoute);
 app.get('*', (req, res) => res.status(200).send(
     {message: 'Welcome to API Test PT ITOMMEY BINTANG INDONESIA'}));
--- controllers/product/createProduct.js
+++ controllers/product/createProduct.js
@@ -1,9 +1,31 @@
+const {tbl_products: productModel} = require('@models');
+const response = require('@helpers/response');
+const {internalServerError} = require('@helpers/errorResponse');
 module.exports = {
   create: async (req, res) => {
     try {
-      console.log(`create`);
+      const {name, qty, expiredAt} = req.body;
+
+      const productModelData = await productModel.create({
+        name: name,
+        qty: qty,
+        expired_at: expiredAt,
+        is_active: true,
+      });
+
+      const result = {
+        id: productModelData.id,
+        name: productModelData.name,
+        qty: productModelData.qty,
+        picture: null,
+        expiredAt: productModelData.expired_at,
+        isActive: productModelData.is_active,
+      };
+
+      return response(res, 200, 'Get Detail Product Success', result);
     } catch (error) {
       console.log(error);
+      return internalServerError(res, error.message);
     }
   },
 };
