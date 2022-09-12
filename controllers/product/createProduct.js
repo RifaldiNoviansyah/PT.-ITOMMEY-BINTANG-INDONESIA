0738e2d3faae2190ee4861d3416e208c38f0d5ea
RIFALDI NOVIANSYAH <rifaldinoviansyah11@gmail.com>
2022-09-12 14:50:31 +0700

    add setupProject

--- controllers/product/createProduct.js
+++ controllers/product/createProduct.js
@@ -7,25 +7,26 @@ module.exports = {
   create: async (req, res) => {
     try {
       const {name, qty, expiredAt} = req.body;
-      await validateInputBody.validateInputBody(res, name, qty, expiredAt);
+      const dataError = await validateInputBody.validateInputBody(res, name, qty, expiredAt);
+      if (!dataError) {
+        const productModelData = await productModel.create({
+          name: name,
+          qty: qty,
+          expired_at: expiredAt,
+          is_active: true,
+        });
 
-      const productModelData = await productModel.create({
-        name: name,
-        qty: qty,
-        expired_at: expiredAt,
-        is_active: true,
-      });
+        const result = {
+          id: productModelData.id,
+          name: productModelData.name,
+          qty: productModelData.qty,
+          picture: null,
+          expiredAt: productModelData.expired_at,
+          isActive: productModelData.is_active,
+        };
 
-      const result = {
-        id: productModelData.id,
-        name: productModelData.name,
-        qty: productModelData.qty,
-        picture: null,
-        expiredAt: productModelData.expired_at,
-        isActive: productModelData.is_active,
-      };
-
-      return response(res, 200, 'Create Product Success', result);
+        return response(res, 200, 'Create Product Success', result);
+      }
     } catch (error) {
       console.log(error);
       return internalServerError(res, error.message);
--- controllers/product/updateProduct.js
+++ controllers/product/updateProduct.js
@@ -7,23 +7,24 @@ module.exports = {
     try {
       const {name, qty, expiredAt} = req.body;
       const productModelData = req.productModelData;
-      await validateInputBody.validateInputBody(res, name, qty, expiredAt);
+      const dataError = await validateInputBody.validateInputBody(res, name, qty, expiredAt);
+      if (!dataError) {
+        productModelData.name = name;
+        productModelData.qty = qty;
+        productModelData.expired_at = expiredAt;
+        await productModelData.save();
 
-      productModelData.name = name;
-      productModelData.qty = qty;
-      productModelData.expired_at = expiredAt;
-      await productModelData.save();
+        const result = {
+          id: productModelData.id,
+          name: productModelData.name,
+          qty: productModelData.qty,
+          picture: null,
+          expiredAt: productModelData.expired_at,
+          isActive: productModelData.is_active,
+        };
 
-      const result = {
-        id: productModelData.id,
-        name: productModelData.name,
-        qty: productModelData.qty,
-        picture: null,
-        expiredAt: productModelData.expired_at,
-        isActive: productModelData.is_active,
-      };
-
-      return response(res, 200, 'Updated Product Success', result);
+        return response(res, 200, 'Updated Product Success', result);
+      }
     } catch (error) {
       console.log(error);
       return internalServerError(res, error.message);
